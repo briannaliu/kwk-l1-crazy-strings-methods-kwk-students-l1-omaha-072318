9a12c2686a809739968fc378f37afeb0aed89462
briannaliu <briannaliu3@gmail.com>
2018-07-25 16:49:16 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,17 +1,12 @@
 # write your method here
 
-def crazy_strings(a,b)
+def crazy_strings("Hello", "Friends")
 
 end
 
 
-# Crazy Strings
 
-That's Nonsense!
+# Write a method `crazy_strings` that takes in two strings as arguments. The first string should be reversed and upcased. The second string should be swapcased and any 's' characters should be replaced with 'z's. Return a string with the first and second words concatenated.
 
-<img src="https://s3.amazonaws.com/after-school-assets/gibberish.gif">
-
-Write a method `crazy_strings` that takes in two strings as arguments. The first string should be reversed and upcased. The second string should be swapcased and any 's' characters should be replaced with 'z's. Return a string with the first and second words concatenated.
-
-For example, `crazy_strings("Hello", "Friends")` should return `"OLLEH fRIENDZ"`.
+# For example, `crazy_strings("Hello", "Friends")` should return `"OLLEH fRIENDZ"`.
 
