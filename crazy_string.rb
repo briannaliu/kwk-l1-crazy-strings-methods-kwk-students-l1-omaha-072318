2daea6caf03279059dd299e77a4d461f089d7ebe
briannaliu <briannaliu3@gmail.com>
2018-07-25 16:50:19 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,13 @@
 # write your method here
 
 def crazy_strings("Hello", "Friends")
-
+  puts "Hello".reverse.upcased
+  puts "Friends"
 end
+
+
+
+# Write a method `crazy_strings` that takes in two strings as arguments. The first string should be reversed and upcased. The second string should be swapcased and any 's' characters should be replaced with 'z's. Return a string with the first and second words concatenated.
+
+# For example, `crazy_strings("Hello", "Friends")` should return `"OLLEH fRIENDZ"`.
+
